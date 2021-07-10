dff2c229e2ceba4ab8fd066b5c277f1447a8a9c5
Sergey <sergios33033@gmail.com>
2021-07-10 18:30:07 +0300

    Fixed, service is able to interact with other microservices.

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,22 +1,21 @@
-import { Controller, Post, Body, UseFilters, HttpCode, HttpStatus, Req } from '@nestjs/common';
-import { ApiCreatedResponse, ApiTags, ApiOperation } from '@nestjs/swagger';
-import { Request } from 'express';
-import { EmailVerificationRequestValidationPipe } from '../pipes/validation/email-verification-request.validation.pipe';
-import { InternalExceptionFilter } from '../exceptions/filters/Internal.exception-filter';
-import { VerifyEmailDto } from './dto/verify-email.dto';
-import { EmailService } from './email.service';
+import { MessagePattern, Payload, Transport } from "@nestjs/microservices";
+import { Controller, UseFilters } from "@nestjs/common";
+import { InternalExceptionFilter } from "../exceptions/filters/Internal.exception-filter";
+import { VerifyEmailDto } from "./dto/verify-email.dto";
+import { EmailService } from "./email.service";
 
 @UseFilters(new InternalExceptionFilter())
-@Controller('email')
-@ApiTags('Email')
+@Controller("email")
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
-  @Post('/verify-email')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: 'Send verification mail.' })
-  @ApiCreatedResponse({})
-  async register(@Req() req: Request, @Body(new EmailVerificationRequestValidationPipe()) verifyEmailDto: VerifyEmailDto) {
+  @MessagePattern({ cmd: "verify-email" }, Transport.REDIS)
+  async validateEmail(@Payload() verifyEmailDto: VerifyEmailDto) {
+    return await this.emailService.validateEmail(verifyEmailDto);
+  }
+  
+  @MessagePattern({ cmd: "reset-password" }, Transport.REDIS)
+  async refreshPasswordEmail(@Payload() verifyEmailDto: VerifyEmailDto) {
     return await this.emailService.validateEmail(verifyEmailDto);
   }
 }
--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { RedisModule } from '../config/redis.module';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
-  imports: [RedisModule],
   controllers: [EmailController],
   providers: [EmailService],
   exports: [EmailService]
