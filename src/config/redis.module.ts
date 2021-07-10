6a1ff278a815c4d0103f1e2b34661cb820fb934a
Sergey <sergios33033@gmail.com>
2021-07-10 18:29:05 +0300

    Removed useless files.

--- src/config/redis.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisConfig } from './redis.config';
-
-@Module({
-  providers: [RedisConfig],
-  exports: [RedisConfig]
-})
-export class RedisModule {}
