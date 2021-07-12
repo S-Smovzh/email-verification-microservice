59f7bad3307be6634626a94ebd1d44bed3ad08be
Sergey <sergios33033@gmail.com>
2021-07-12 15:21:40 +0300

    Added the microservice bootstrap method.

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,54 @@
-// import middleware from 'i18next-http-middleware';
-import { lstatSync, readdirSync } from 'fs';
-import { NestFactory } from '@nestjs/core';
-import { join, resolve } from 'path';
-import i18next from 'i18next';
-import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import "reflect-metadata";
 
-const Backend = require('i18next-fs-backend');
+// const Backend = require("i18next-fs-backend");
+//
+// async function bootstrap() {
+//   const directory = resolve(process.cwd(), "dist/locales");
+//
+//   await i18next.use(Backend).init(
+//     {
+//       // initImmediate: false,
+//       // debug:true,
+//       lng: "en",
+//       fallbackLng: "en",
+//       preload: readdirSync(directory).filter((fileName) => {
+//         const joinedPath = join(directory, fileName);
+//         return lstatSync(joinedPath).isDirectory();
+//       }),
+//       backend: {
+//         loadPath: join(__dirname, "/locales/{{lng}}.json")
+//       }
+//     },
+//     (e) => {
+//       if (e) {
+//         return console.error(e);
+//       }
+//     }
+//   );
+//
+//   const app = await NestFactory.create(AppModule);
+//   app.use(i18next);
+//
+//   await app.listen(process.env.PORT || 7000);
+// }
+//
+// bootstrap();
+//
 
 async function bootstrap() {
-  const directory = resolve(process.cwd(), 'dist/locales');
-
-  i18next.use(Backend).init(
-    {
-      // initImmediate: false,
-      // debug:true,
-      lng: 'en',
-      fallbackLng: 'en',
-      preload: readdirSync(directory).filter((fileName) => {
-        const joinedPath = join(directory, fileName);
-        return lstatSync(joinedPath).isDirectory();
-      }),
-      backend: {
-        loadPath: join(__dirname, '/locales/{{lng}}.json')
-      }
-    },
-    (e) => {
-      if (e) {
-        return console.error(e);
-      }
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      url: `redis://${process.env.REDIS_DB_NAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_ENDPOINT}:${process.env.REDIS_PORT}`,
+      retryDelay: 3000,
+      retryAttempts: 10
     }
-  );
-
-  const app = await NestFactory.create(AppModule);
-  // app.use(i18next);
-  app.enableCors({
-    origin: [process.env.AUTHENTICATION_MICROSERVICE_URL, '*'],
-    credentials: true,
-    methods: ['POST']
   });
-
-  await app.listen(process.env.PORT || 7000);
+  
+  app.listen(() => console.log("Microservice is listening"));
 }
 
 bootstrap();
