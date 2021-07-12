7b67818052d382d17dbff5aa4cae8917f13fd9f7
Sergey <sergios33033@gmail.com>
2021-07-12 15:24:44 +0300

    Useless error code removed.

--- src/exceptions/GlobalErrorCodes.ts
+++ src/exceptions/GlobalErrorCodes.ts
@@ -1,6 +1,5 @@
 export class GlobalErrorCodes {
   static readonly INTERNAL_ERROR = new GlobalErrorCodes('INTERNAL_ERROR', 600, 'Internal Failure!');
-  static readonly EMPTY_ERROR = new GlobalErrorCodes('EMPTY_ERROR', 601, "This field mustn't be empty.");
 
   private constructor(private readonly key: string, public readonly code: number, public readonly value: string) {}
 
