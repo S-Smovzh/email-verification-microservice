b3c4e398cf9086acfba685f3ce02e47c7261f3c3
Sergey <sergios33033@gmail.com>
2021-08-15 20:57:03 +0300

    Fix bug.

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,18 +1,18 @@
 import { MessagePattern, Payload, Transport } from "@nestjs/microservices";
-import { Controller, UseFilters } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, UseFilters } from "@nestjs/common";
 import { ExceptionFilter } from "../exceptions/Exception.filter";
 import { VerifyDataDto } from "./dto/verify-data.dto";
 import { EmailService } from "./email.service";
 
 @UseFilters(new ExceptionFilter())
-@Controller("email")
+@Controller()
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
-  @MessagePattern({ cmd: "invoke" }, Transport.REDIS)
-  async invoke(): Promise<void> {
-    console.log("email-ver-service invoked");
-  }
+  // @MessagePattern({ cmd: "invoke" }, Transport.REDIS)
+  // async invoke(): Promise<void> {
+  //   console.log("email-ver-service invoked");
+  // }
 
   @MessagePattern({ cmd: "verify" }, Transport.REDIS)
   async validateEmail(@Payload() verifyEmailDto: VerifyDataDto) {
@@ -22,5 +22,11 @@ export class EmailController {
   @MessagePattern({ cmd: "reset-password" }, Transport.REDIS)
   async refreshPasswordEmail(@Payload() verifyEmailDto: VerifyDataDto) {
     return await this.emailService.validateEmail(verifyEmailDto);
+  }
+  
+  @Get("/")
+  @HttpCode(HttpStatus.OK)
+  async invoke(): Promise<void> {
+    console.log("ver-service invoked");
   }
 }
