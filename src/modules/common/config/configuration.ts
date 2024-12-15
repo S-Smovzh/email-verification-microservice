5d40cddc308cdcb18c3d09992110f94bb1567589
s.smovzh <sergios33033@gmail.com>
2024-12-15 16:51:04 +0200

    fix: update lib

--- src/modules/common/config/configuration.ts
+++ src/modules/common/config/configuration.ts
@@ -1,17 +1,18 @@
 import * as process from "process";
 import * as dotenv from "dotenv";
+import { AppConfigInterface, RabbitConfigInterface } from "@ssmovzh/chatterly-common-utils";
 
 dotenv.config();
 
 export default () => ({
   app: {
-    port: process.env.PORT,
+    port: +process.env.PORT,
     environment: process.env.ENVIRONMENT,
     clientUrl: process.env.CLIENT_URL
-  },
+  } as AppConfigInterface,
   mailConfig: {
     host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    port: +process.env.MAIL_PORT,
     user: process.env.MAIL_USER,
     password: process.env.MAIL_PASSWORD,
     senderName: process.env.SENDER_NAME,
@@ -23,5 +24,5 @@ export default () => ({
     port: process.env.RABBIT_PORT ? +process.env.RABBIT_PORT : 5672,
     username: process.env.RABBIT_USERNAME,
     password: process.env.RABBIT_PASSWORD
-  }
+  } as RabbitConfigInterface
 });
